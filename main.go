4d193f226c96c741e00d0b115c5812bb23ac351c
kei <kei.takakuda.0415@icloud.com>
2020-02-09 16:20:03 +0900

    sugar

--- main.go
+++ main.go
@@ -9,4 +9,8 @@ func main() {
 	logger, _ := zap.NewDevelopment()
 	logger.Info("Hello zap", zap.String("key", "value"), zap.Time("now", time.Now()))
 	logger.Debug("msg", zap.String("key", "string"), zap.Ints("ints", []int{10, 20}))
+	sugar := zap.NewExample().Sugar()
+	sugar.Info("one", "two", "three")
+	sugar.Infof("one: %s, %d", "two", 10)
+	sugar.Infow("msg", "key", "value", "intArray", []int{10, 100}, "duration", time.Second*200)
 }
